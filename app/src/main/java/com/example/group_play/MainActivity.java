92980c7fb18cb818a3a622f0f8b8f7fcf7b5b245
parshva-b <parshva.barbhaya@somaiya.edu>
2019-03-09 19:31:07 +0530

    API initialization

--- app/src/main/java/com/example/group_play/MainActivity.java
+++ app/src/main/java/com/example/group_play/MainActivity.java
@@ -1,23 +1,54 @@
 package com.example.group_play;
 
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final IntentFilter intentFilter = new IntentFilter();
+    Channel channel;
+    WifiP2pManager manager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
     }
 
     public void send(View view) {
-        Toast.makeText(this,"Sending works", Toast.LENGTH_LONG);
+//        Log.v("MainAcitivity","Send");
+        Toast toast = Toast.makeText(this,"Sending works", Toast.LENGTH_LONG);
+        toast.show();
+//        getSystemService(WIFI_P2P_SERVICE);
+
     }
 
     public void receive(View view) {
-        Toast.makeText(this,"Recieving works", Toast.LENGTH_LONG);
+//        Log.v("MainActivity", "Receive");
+        Toast toast = Toast.makeText(this,"Recieving works", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
